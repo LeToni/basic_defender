17f14724183b53491ddde6cfb9231ffd4354e5af
Toni Le <toni.le@outlook.de>
2021-02-26 19:01:20 +0100

    Move square with arrow keys

--- src/lib.rs
+++ src/lib.rs
@@ -2,42 +2,55 @@ pub mod config;
 pub mod utilities;
 
 use config::GraphicsConfig;
-use piston::input::{RenderArgs, UpdateArgs};
+use piston::input::*;
+use piston::window::Window;
+use piston::Button;
 use utilities::colors;
 
 pub struct App {
     pub window: GraphicsConfig,
-    rotation: f64,
+    pub x: f64,
+    pub y: f64,
 }
 
 impl App {
     pub fn new(window: GraphicsConfig) -> App {
-        App {
-            window,
-            rotation: 0.0,
+        let size = window.settings.size();
+        let (x, y) = ((size.width / 2.0), (size.height / 2.0));
+
+        App { window, x, y }
+    }
+
+    pub fn input(&mut self, button: Button) {
+        if let Button::Keyboard(key) = button {
+            match key {
+                Key::Up => self.y = self.y - 10.0,
+                Key::Down => self.y = self.y + 10.0,
+                Key::Left => self.x = self.x - 10.0,
+                Key::Right => self.x = self.x + 10.0,
+                _ => (),
+            }
         }
     }
 
     pub fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+        let square = rectangle::square(0.0, 0.0, 15.0);
+        let (x, y) = (self.x, self.y);
+        // let rotation = self.rotation;
         self.window.gl.draw(args.viewport(), |c, gl| {
             clear(colors::BLACK, gl);
 
-            let transform = c
-                .transform
-                .trans(x, y)
-                .rot_rad(rotation)
-                .trans(-25.0, -25.0);
+            let transform = c.transform.trans(x, y);
+            // .rot_rad(rotation)
+            // .trans(-25.0, -25.0);
 
             rectangle(colors::RED, square, transform, gl);
         });
     }
 
-    pub fn update(&mut self, args: &UpdateArgs) {
-        self.rotation += 2.0 * args.dt;
-    }
+    // pub fn update(&mut self, args: &UpdateArgs) {
+    //     // self.rotation += 2.0 * args.dt;
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderEvent, UpdateEvent};
+use piston::PressEvent;
 
 use defender::config::GraphicsConfig;
 use defender::App;
 
 fn main() {
-    let window = GraphicsConfig::new("Basic Defender", 200, 200);
+    let window = GraphicsConfig::new("Basic Defender", 960, 768);
     // Create a new game and run it.
     let mut app = App::new(window);
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut app.window.settings) {
+        if let Some(key) = e.press_args() {
+            app.input(key);
+        }
         if let Some(args) = e.render_args() {
             app.render(&args);
-        }
-
-        if let Some(args) = e.update_args() {
-            app.update(&args);
         }
     }
 }

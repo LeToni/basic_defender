1d2f3fe28e3134139474c1d0b1e3f780650d0416
Toni Le <toni.le@outlook.de>
2021-03-02 19:34:15 +0100

    Fix model Bullet issues

--- src/models/bullet.rs
+++ src/models/bullet.rs
@@ -17,11 +17,42 @@ pub struct Bullet {
 }
 
 impl Bullet {
-    pub fn new {x: f64, y: f64, dir: geom::Direction} -> Bullet{
-        Bullet{pos: geom::Position{x,y}, dir,size: BULLET_SIZE,ttl: BULLET_LIFETIME}
+    pub fn new(x: f64, y: f64, dir: geom::Direction) -> Bullet {
+        Bullet {
+            pos: geom::Position { x, y },
+            dir,
+            size: BULLET_SIZE,
+            ttl: BULLET_LIFETIME,
+        }
     }
 
-    pub fn radius(&self) -> f64{
-        self.size/2.0
+    pub fn radius(&self) -> f64 {
+        self.size / 2.0
+    }
+}
+
+impl GameObject for Bullet {
+    fn position(&self) -> &geom::Position {
+        &self.pos
+    }
+    fn radius(&self) -> f64 {
+        BULLET_SIZE
+    }
+
+    fn render(&self, context: &Context, gl: &mut GlGraphics) {
+        let transform = context.transform.trans(self.pos.x, self.pos.y);
+        let radius = self.radius();
+        ellipse(colors::WHITE, [0.0, 0.0, radius, radius], transform, gl);
+    }
+
+    fn update(&mut self, dt: f64, _: Size) {
+        self.ttl = self.ttl - dt;
+        // Move the bullet in the direction the player was facing.
+        match self.dir {
+            geom::Direction::EAST => self.pos.x = self.pos.x + BULLET_SPEED,
+            geom::Direction::NORTH => self.pos.y = self.pos.y - BULLET_SPEED,
+            geom::Direction::WEST => self.pos.x = self.pos.x - BULLET_SPEED,
+            geom::Direction::SOUTH => self.pos.y = self.pos.y + BULLET_SPEED,
+        }
     }
 }

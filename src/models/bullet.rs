600950f1af2a0bdccdd2c361889fbf0aa4f738a4
Toni Le <toni.le@outlook.de>
2021-03-02 19:26:09 +0100

    Bullet structure

--- src/models/bullet.rs
+++ src/models/bullet.rs
@@ -1,1 +1,27 @@
+use super::GameObject;
+use crate::colors;
+use crate::geom;
+use graphics::{ellipse, Context, Transformed};
+use opengl_graphics::GlGraphics;
+use piston::window::Size;
 
+const BULLET_SPEED: f64 = 2.0;
+const BULLET_SIZE: f64 = 5.0;
+const BULLET_LIFETIME: f64 = 2.0;
+
+pub struct Bullet {
+    pub pos: geom::Position,
+    pub dir: geom::Direction,
+    pub size: f64,
+    pub ttl: f64,
+}
+
+impl Bullet {
+    pub fn new {x: f64, y: f64, dir: geom::Direction} -> Bullet{
+        Bullet{pos: geom::Position{x,y}, dir,size: BULLET_SIZE,ttl: BULLET_LIFETIME}
+    }
+
+    pub fn radius(&self) -> f64{
+        self.size/2.0
+    }
+}

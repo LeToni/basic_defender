d157c4c5f3848d0cb30dc81978a020c267923719
Toni Le <toni.le@outlook.de>
2021-03-03 21:37:29 +0100

    Render bullets

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,29 @@ pub mod config;
 pub mod geom;
 pub mod models;
 
-use crate::models::GameObject;
-// use crate::models::bullet::Bullet;
+use crate::models::bullet::Bullet;
 use crate::models::enemy::Enemy;
 use crate::models::ship::Ship;
+use crate::models::GameObject;
 use config::GraphicsConfig;
 use piston::input::*;
 use piston::window::Window;
 use piston::Button;
 
 pub const UNIT_MOVE: f64 = 10.0;
+pub const FIRE_COOLDOWN: f64 = 0.1;
+
+pub struct GameState {
+    fire_bullets: bool,
+    fire_cooldown: f64,
+}
 
 pub struct App {
     pub window: GraphicsConfig,
     pub ship: Ship,
     enemies: Vec<Enemy>,
-    // bullets: Vec<Bullet>,
+    bullets: Vec<Bullet>,
+    state: GameState,
 }
 
 impl App {
@@ -27,10 +34,16 @@ impl App {
         let (x, y) = ((size.width / 2.0), (size.height / 2.0));
 
         let ship = Ship::new(x, y);
+        let state = GameState {
+            fire_bullets: false,
+            fire_cooldown: 0.0,
+        };
         App {
             window,
             ship,
             enemies: Vec::new(),
+            bullets: Vec::new(),
+            state,
         }
     }
 
@@ -42,6 +55,12 @@ impl App {
                     Key::Down => self.ship.start_move(geom::Direction::SOUTH),
                     Key::Left => self.ship.start_move(geom::Direction::WEST),
                     Key::Right => self.ship.start_move(geom::Direction::EAST),
+                    Key::Space => {
+                        if self.state.fire_cooldown <= 0.0 {
+                            self.state.fire_cooldown = FIRE_COOLDOWN;
+                            self.state.fire_bullets = true;
+                        }
+                    }
                     _ => (),
                 }
             }
@@ -59,10 +78,15 @@ impl App {
     pub fn render(&mut self, args: &RenderArgs) {
         let ship = &self.ship;
         let enemies = &self.enemies;
+        let bullets = &self.bullets;
 
         self.window.gl.draw(args.viewport(), |c, gl| {
             use graphics::*;
             clear(colors::BLACK, gl);
+
+            for bullet in bullets.iter() {
+                bullet.render(&c, gl);
+            }
 
             for enemy in enemies.iter() {
                 enemy.render(&c, gl);
@@ -74,6 +98,21 @@ impl App {
 
     pub fn update(&mut self, args: UpdateArgs) {
         let size = self.window.size;
+
+        if self.state.fire_cooldown > 0.0 {
+            self.state.fire_cooldown = self.state.fire_cooldown - args.dt;
+        }
+
+        if self.state.fire_bullets {
+            self.state.fire_bullets = false;
+            self.bullets
+                .push(Bullet::new(self.ship.pos.x, self.ship.pos.y, self.ship.dir));
+        }
+
+        for bullet in &mut self.bullets {
+            bullet.update(args.dt, &size);
+        }
+
         self.ship.update(args.dt, &size);
 
         if self.enemies.is_empty() {
@@ -81,5 +120,7 @@ impl App {
                 self.enemies.push(Enemy::new_rand(size.width, size.height));
             }
         }
+
+        self.bullets.retain(|bullet| bullet.ttl > 0.0);
     }
 }
